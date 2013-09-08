b2aeaee9931544852970c103232d028f53f00104
Marc Durdin <marc@durdin.net>
2013-09-09 09:04:04 +1000

    layout, bootstrap

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ Mesmeride::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  # Speed up tests by lowering bcrypt's cost function.
+  ActiveModel::SecurePassword.min_cost = true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Mesmeride::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
+  root 'static_pages#home'
+  match '/help', to: 'static_pages#help', via: 'get'
+  match '/about', to: 'static_pages#about', via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
