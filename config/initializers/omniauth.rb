7be4e3469a4911f64f847e074e4142a22dca5785
Marc Durdin <marc@durdin.net>
2013-09-14 07:07:14 +1000

    strava key via environment

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,12 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 
   def secret_key
-    token_file = Rails.root.join('.strava_secret_key')
-    File.read(token_file).chomp
+    ENV['STRAVA_SECRET_KEY']
+#    ||
+#    begin
+#      token_file = Rails.root.join('.strava_secret_key')
+#      File.read(token_file).chomp
+#    end
   end
 
   provider :strava, '81', secret_key, {client_options: {ssl: {ca_file: Rails.root.join('lib/assets/cacert.pem').to_s}}}
