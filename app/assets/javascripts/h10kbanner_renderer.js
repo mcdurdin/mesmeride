42f4de27cc860e6fea3ffb8b52c7f9bd8a8fac92
Marc Durdin <marc@durdin.net>
2013-09-16 23:23:50 +1000

    cleanup gradient fill in h10k

--- app/assets/javascripts/h10kbanner_renderer.js
+++ app/assets/javascripts/h10kbanner_renderer.js
@@ -278,8 +278,6 @@ window.mesmeride.h10KBannerRenderer = {
         
         var x1 = s.startX, y1 = s.startY, x2 = s.endX, y2 = s.endY;
         
-        if(y2 == y1) continue;
-        
         context.moveTo(x1, y1);     // x1 y1  x2 y2 -->   (y2-y1)/(x2-x1) * (x2-x1+2) + y2
         context.lineTo(x1, ysubbase);
         context.lineTo(x2, ysubbase);
